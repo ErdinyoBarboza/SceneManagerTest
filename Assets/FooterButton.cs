f388400d3ac8905e6fd1662af16d39b13348a4f8
sanukin39 <sanukin39@gmail.com>
2016-03-03 21:49:26 +0900

    first-commit

---
+++ Assets/FooterButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class FooterButton : MonoBehaviour {
+    
+    public MainScene scene;
+    
+    void Start(){
+        GetComponent<Button>().onClick.AddListener(LoadScene);
+    }    
+    
+    public void LoadScene(){
+        FindObjectOfType<SceneHolder>().LoadMainScene(scene);
+    }
+}
+
+
---
+++ Assets/SceneHolder.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneHolder : MonoBehaviour {
+    
+    Scene LoadedScene;
+    
+    void Start(){
+        LoadMainScene(MainScene.Quest);
+    }
+	
+    public void LoadMainScene(MainScene scene){
+        SceneManager.UnloadScene(LoadedScene.name);
+        SceneManager.LoadScene(scene.ToString(), LoadSceneMode.Additive);
+        LoadedScene = SceneManager.GetSceneByName(scene.ToString());
+    }
+}
+
+public enum MainScene{
+    Friend,
+    Gacha,
+    Monster,
+    Quest,
+    Shop
+}
